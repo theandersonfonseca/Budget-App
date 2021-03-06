303d8055757e37cbd921dc7e8c22c8883ae97813
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-03-06 17:30:54 -0300

    change the defer attribute to the module attribute and refactor js code

--- src/js/budget-class.js
+++ src/js/budget-class.js
@@ -1,6 +1,6 @@
 import { convertToMoney } from './utils.js';
 
-export default class Budget {
+export class Budget {
   constructor(
     incomeDisplay,
     expenseDisplay,
@@ -23,12 +23,14 @@ export default class Budget {
     this._graphic = graphic;
   }
 
-  _saveDataToLocalStorage() {
-    localStorage.setItem('incomeHistory', JSON.stringify(this._incomeHistory));
-    localStorage.setItem(
-      'expenseHistory',
-      JSON.stringify(this._expenseHistory)
-    );
+  updateDataFromLocalStorage() {
+    if (localStorage.getItem('incomeHistory') === null) return;
+
+    this._incomeHistory = JSON.parse(localStorage.getItem('incomeHistory'));
+    this._expenseHistory = JSON.parse(localStorage.getItem('expenseHistory'));
+
+    this._updateAmountsTotal('income');
+    this._updateAmountsTotal('expense');
   }
 
   setIncomeOrExpense(incomeOrExpense, name, amount) {
@@ -37,84 +39,6 @@ export default class Budget {
       : this._expenseHistory.push({ name, amount });
 
     this._updateAmountsTotal(incomeOrExpense);
-  }
-
-  _removeIncomeOrExpense(incomeOrExpense, name) {
-    incomeOrExpense === 'income'
-      ? (this._incomeHistory = this._incomeHistory.filter(
-          (el) => el.name !== name
-        ))
-      : (this._expenseHistory = this._expenseHistory.filter(
-          (el) => el.name !== name
-        ));
-
-    this._updateAmountsTotal(incomeOrExpense);
-  }
-
-  _addClickEventOnRemoveButtons() {
-    const removeBtns = document.querySelectorAll('[data-remove]');
-
-    removeBtns.forEach((btn) =>
-      btn.addEventListener('click', (e) => {
-        const transactionName =
-          e.target.previousElementSibling.children[0].innerHTML;
-
-        e.target.dataset.remove === 'income'
-          ? this._removeIncomeOrExpense('income', transactionName)
-          : this._removeIncomeOrExpense('expense', transactionName);
-      })
-    );
-  }
-
-  _calculateIncomeAndExpenseRatio() {
-    const percentage =
-      this._balanceAmount <= 0
-        ? 100 -
-          (this._incomeAmount / (this._expenseAmount + this._incomeAmount)) *
-            100
-        : (this._expenseAmount / (this._expenseAmount + this._incomeAmount)) *
-          100;
-
-    return Math.ceil(percentage);
-  }
-
-  _createElements(incomeOrExpense, name, amount) {
-    const div = document.createElement('div');
-    div.classList.add('history__push-container');
-    div.setAttribute(`data-${incomeOrExpense}`, '');
-
-    div.innerHTML = `<div class="history__push-descriptions history__push-descriptions--${incomeOrExpense}">
-                        <span class="history__push-text">${name}</span>
-                        <span class="history__push-amount" data-amount="${amount}">
-                          ${convertToMoney(amount)}
-                        </span>
-                      </div>
-  
-                      <i class="fas fa-trash history__remove-icon" data-remove="${incomeOrExpense}"></i>
-                      `;
-
-    return div;
-  }
-
-  _addElementsToDom(amount, display, history, historyDisplay, incomeOrExpense) {
-    historyDisplay.innerHTML = '';
-
-    display.innerHTML = convertToMoney(amount);
-
-    history.forEach((el) => {
-      historyDisplay.appendChild(
-        this._createElements(incomeOrExpense, el.name, el.amount)
-      );
-    });
-
-    this._balanceDisplay.innerHTML = convertToMoney(this._balanceAmount);
-
-    this._graphic.classList.remove([...this._graphic.classList][1]);
-    this._graphic.classList.add(
-      `balance__graphic--${this._calculateIncomeAndExpenseRatio()}`
-    );
-
-    this._addClickEventOnRemoveButtons();
   }
 
   _updateAmountsTotal(incomeOrExpense) {
@@ -139,21 +63,97 @@ export default class Budget {
     this._saveDataToLocalStorage();
   }
 
+  _saveDataToLocalStorage() {
+    localStorage.setItem('incomeHistory', JSON.stringify(this._incomeHistory));
+    localStorage.setItem(
+      'expenseHistory',
+      JSON.stringify(this._expenseHistory)
+    );
+  }
+
+  _addElementsToDom(amount, display, history, historyDisplay, incomeOrExpense) {
+    historyDisplay.innerHTML = '';
+
+    display.innerHTML = convertToMoney(amount);
+
+    history.forEach((el) => {
+      historyDisplay.appendChild(
+        this._createElements(incomeOrExpense, el.name, el.amount)
+      );
+    });
+
+    this._balanceDisplay.innerHTML = convertToMoney(this._balanceAmount);
+
+    this._graphic.classList.remove([...this._graphic.classList][1]);
+    this._graphic.classList.add(
+      `balance__graphic--${this._calculateIncomeAndExpenseRatio()}`
+    );
+
+    this._addClickEventOnRemoveButtons();
+  }
+
+  _createElements(incomeOrExpense, name, amount) {
+    const div = document.createElement('div');
+    div.classList.add('history__push-container');
+    div.setAttribute(`data-${incomeOrExpense}`, '');
+
+    div.innerHTML = `<div class="history__push-descriptions history__push-descriptions--${incomeOrExpense}">
+                        <span class="history__push-text">${name}</span>
+                        <span class="history__push-amount" data-amount="${amount}">
+                          ${convertToMoney(amount)}
+                        </span>
+                      </div>
+  
+                      <i class="fas fa-trash history__remove-icon" data-remove="${incomeOrExpense}"></i>
+                      `;
+
+    return div;
+  }
+
+  _addClickEventOnRemoveButtons() {
+    const removeBtns = document.querySelectorAll('[data-remove]');
+
+    removeBtns.forEach((btn) =>
+      btn.addEventListener('click', (e) => {
+        const transactionName =
+          e.target.previousElementSibling.children[0].innerHTML;
+
+        e.target.dataset.remove === 'income'
+          ? this._removeIncomeOrExpense('income', transactionName)
+          : this._removeIncomeOrExpense('expense', transactionName);
+      })
+    );
+  }
+
+  _removeIncomeOrExpense(incomeOrExpense, name) {
+    incomeOrExpense === 'income'
+      ? (this._incomeHistory = this._incomeHistory.filter(
+          (el) => el.name !== name
+        ))
+      : (this._expenseHistory = this._expenseHistory.filter(
+          (el) => el.name !== name
+        ));
+
+    this._updateAmountsTotal(incomeOrExpense);
+  }
+
+  _calculateIncomeAndExpenseRatio() {
+    const percentage =
+      this._balanceAmount <= 0
+        ? 100 -
+          (this._incomeAmount / (this._expenseAmount + this._incomeAmount)) *
+            100
+        : (this._expenseAmount / (this._expenseAmount + this._incomeAmount)) *
+          100;
+
+    return Math.ceil(percentage);
+  }
+
   checkIfNameAlreadyExists(incomeOrExpense, name) {
     if (incomeOrExpense === 'income') {
       return this._incomeHistory.some((el) => el.name === name);
     } else {
       return this._expenseHistory.some((el) => el.name === name);
     }
-  }
-
-  updateDataFromLocalStorage() {
-    if (localStorage.getItem('incomeHistory') === null) return;
-
-    this._incomeHistory = JSON.parse(localStorage.getItem('incomeHistory'));
-    this._expenseHistory = JSON.parse(localStorage.getItem('expenseHistory'));
-
-    this._updateAmountsTotal('income');
-    this._updateAmountsTotal('expense');
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,78 +1,75 @@
-import Budget from './budget-class.js';
+import { Budget } from './budget-class.js';
 import { el } from './dom-elements.js';
 
-const init = (() => {
-  const newBudget = new Budget(
-    el.incomeDisplay,
-    el.expenseDisplay,
-    el.balanceDisplay,
-    el.incomeHistoryDisplay,
-    el.expenseHistoryDisplay,
-    el.graphic
+const newBudget = new Budget(
+  el.incomeDisplay,
+  el.expenseDisplay,
+  el.balanceDisplay,
+  el.incomeHistoryDisplay,
+  el.expenseHistoryDisplay,
+  el.graphic
+);
+
+const isInputValid = (incomeOrExpense) => {
+  const isIncomeInputsEmpty =
+    !el.inputNameIncome.value.trim() || !el.inputTransactionIncome.value.trim();
+  const isExpenseInputsEmpty =
+    !el.inputNameExpense.value.trim() ||
+    !el.inputTransactionExpense.value.trim();
+
+  const nameAlreadyExistsMsg = 'Esse nome já existe, escolha outra!';
+  const emptyInputMsg = 'Preencha todos os campos adequadamente!';
+
+  if (incomeOrExpense === 'income' && isIncomeInputsEmpty) {
+    window.alert(emptyInputMsg);
+    return;
+  }
+
+  if (incomeOrExpense === 'expense' && isExpenseInputsEmpty) {
+    window.alert(emptyInputMsg);
+    return;
+  }
+
+  if (
+    newBudget.checkIfNameAlreadyExists('income', el.inputNameIncome.value) ||
+    newBudget.checkIfNameAlreadyExists('expense', el.inputNameExpense.value)
+  ) {
+    window.alert(nameAlreadyExistsMsg);
+    return;
+  }
+
+  return true;
+};
+
+const cleanInputs = () => {
+  [
+    el.inputNameIncome,
+    el.inputTransactionIncome,
+    el.inputNameExpense,
+    el.inputTransactionExpense,
+  ].forEach((input) => (input.value = ''));
+};
+
+const handleIncomeOrExpense = (incomeOrExpense) => {
+  newBudget.setIncomeOrExpense(
+    incomeOrExpense,
+    incomeOrExpense === 'income'
+      ? el.inputNameIncome.value
+      : el.inputNameExpense.value,
+    incomeOrExpense === 'income'
+      ? +el.inputTransactionIncome.value
+      : +el.inputTransactionExpense.value
   );
+};
 
-  const isInputValid = (incomeOrExpense) => {
-    const isIncomeInputsEmpty =
-      !el.inputNameIncome.value.trim() ||
-      !el.inputTransactionIncome.value.trim();
-    const isExpenseInputsEmpty =
-      !el.inputNameExpense.value.trim() ||
-      !el.inputTransactionExpense.value.trim();
+el.submitInputsBtn.forEach((btn) =>
+  btn.addEventListener('click', (e) => {
+    const incomeOrExpense = e.target.dataset.btn;
+    e.preventDefault();
+    if (!isInputValid(incomeOrExpense)) return;
+    handleIncomeOrExpense(incomeOrExpense);
+    cleanInputs();
+  })
+);
 
-    const nameAlreadyExistsMsg = 'Esse nome já existe, escolha outra!';
-    const emptyInputMsg = 'Preencha todos os campos adequadamente!';
-
-    if (incomeOrExpense === 'income' && isIncomeInputsEmpty) {
-      window.alert(emptyInputMsg);
-      return;
-    }
-
-    if (incomeOrExpense === 'expense' && isExpenseInputsEmpty) {
-      window.alert(emptyInputMsg);
-      return;
-    }
-
-    if (
-      newBudget.checkIfNameAlreadyExists('income', el.inputNameIncome.value) ||
-      newBudget.checkIfNameAlreadyExists('expense', el.inputNameExpense.value)
-    ) {
-      window.alert(nameAlreadyExistsMsg);
-      return;
-    }
-
-    return true;
-  };
-
-  const cleanInputs = () => {
-    [
-      el.inputNameIncome,
-      el.inputTransactionIncome,
-      el.inputNameExpense,
-      el.inputTransactionExpense,
-    ].forEach((input) => (input.value = ''));
-  };
-
-  const handleIncomeOrExpense = (incomeOrExpense) => {
-    newBudget.setIncomeOrExpense(
-      incomeOrExpense,
-      incomeOrExpense === 'income'
-        ? el.inputNameIncome.value
-        : el.inputNameExpense.value,
-      incomeOrExpense === 'income'
-        ? +el.inputTransactionIncome.value
-        : +el.inputTransactionExpense.value
-    );
-  };
-
-  el.submitInputsBtn.forEach((btn) =>
-    btn.addEventListener('click', (e) => {
-      const incomeOrExpense = e.target.dataset.btn;
-      e.preventDefault();
-      if (!isInputValid(incomeOrExpense)) return;
-      handleIncomeOrExpense(incomeOrExpense);
-      cleanInputs();
-    })
-  );
-
-  newBudget.updateDataFromLocalStorage();
-})();
+newBudget.updateDataFromLocalStorage();
